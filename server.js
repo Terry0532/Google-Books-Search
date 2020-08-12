fcb38ee51282d5486dc6a02f6358e906581d08cf
Terry <t3rrygong@gmail.com>
2020-08-12 16:48:56 -0600

    heroku

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ const app = express();
 const routes = require("./routes");
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
+const path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
+
+app.use(express.static(path.join(__dirname, "client", "build")))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 db.sequelize.sync().then(function () {
     app.listen(PORT, () => console.log("App listening on PORT: " + PORT));
