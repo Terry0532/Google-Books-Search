7375862045bde68ff333d975faf7e310e5f0cee5
Terry <t3rrygong@gmail.com>
2020-08-20 19:47:29 -0600

    uninstall body parser

--- server.js
+++ server.js
@@ -4,25 +4,18 @@ const routes = require("./routes");
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
 const path = require("path");
-const bodyParser = require("body-parser");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-app.use(function (req, res, next) {
-    res.header(
-        "Access-Control-Allow-Headers",
-        "x-access-token, Origin, Content-Type, Accept"
-    );
-    next();
-});
-
-// parse requests of content-type - application/json
-app.use(bodyParser.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(function (req, res, next) {
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "x-access-token, Origin, Content-Type, Accept"
+//     );
+//     next();
+// });
 
 app.use(express.static(path.join(__dirname, "client", "build")))
 app.get("*", (req, res) => {
